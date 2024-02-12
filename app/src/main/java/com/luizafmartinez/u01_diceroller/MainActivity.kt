3b897d0f4e416bed750a2fd5d9121012fed7cc71
alxmart <alxmartinez@gmail.com>
2024-02-12 12:59:56 -0300

    Choose the right drawable resource based on the value of randomInt.

--- app/src/main/java/com/luizafmartinez/u01_diceroller/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/u01_diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.luizafmartinez.u01_diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.Random
@@ -25,9 +26,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
+        //val resultText: TextView = findViewById(R.id.result_text)
+        //resultText.setText(ramdomInt.toString())
+        val diceImage: ImageView = findViewById(R.id.dice_image)
         val ramdomInt = Random().nextInt(6) + 1
-        resultText.setText(ramdomInt.toString())
+
+        val drawableResource = when (ramdomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
 
