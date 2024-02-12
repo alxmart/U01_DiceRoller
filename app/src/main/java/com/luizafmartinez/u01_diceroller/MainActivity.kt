e541711c262975721bc0d0101e263ecfbdde2baa
alxmart <alxmartinez@gmail.com>
2024-02-12 11:20:24 -0300

    Linking the button.

--- app/src/main/java/com/luizafmartinez/u01_diceroller/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/u01_diceroller/MainActivity.kt
@@ -2,10 +2,36 @@ package com.luizafmartinez.u01_diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       // val rollButton = findViewById<Button>(R.id.roll_button)
+        val rollButton: Button = findViewById(R.id.roll_button)
+
+        // rollButton.setText("Roll the bones")
+        rollButton.text = "Let's Roll"
+
+        rollButton.setOnClickListener {
+
+            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
