d026ec22b2cfb4c168bcdf120057bb830a052e5e
alxmart <alxmartinez@gmail.com>
2024-02-12 12:54:41 -0300

    Added dice images and replace TextView with ImageView.

--- app/src/main/java/com/luizafmartinez/u01_diceroller/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/u01_diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.luizafmartinez.u01_diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,9 +19,15 @@ class MainActivity : AppCompatActivity() {
         rollButton.text = "Let's Roll"
 
         rollButton.setOnClickListener {
-
-            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val ramdomInt = Random().nextInt(6) + 1
+        resultText.setText(ramdomInt.toString())
     }
 }
 
